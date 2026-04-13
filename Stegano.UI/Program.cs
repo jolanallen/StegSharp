df8f93cfd83841bbef57d91e3f98cd8301c3e25e
jolan <jolanallen34@gmail.com>
2026-04-13 15:37:55 +0200

    feat: création d'un logger dans le moteur SteganoUi et instanciation

--- Stegano.UI/Program.cs
+++ Stegano.UI/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Avalonia;
 using System;
 
+using Serilog;
+
+
 namespace Stegano.UI;
 
 class Program
@@ -9,8 +12,29 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args) {
+    Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Debug()
+        .WriteTo.Console()
+        .CreateLogger();
+    try
+        {
+            Log.Information("Démarrage de l'application Stegano.UI");
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "L'application s'est terminée de manière inattendue");
+        }
+        finally
+        {
+            Log.Information("Fermeture de l'application Stegano.UI");
+            Log.CloseAndFlush();
+        }
+    
+    }
+        
+        
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
